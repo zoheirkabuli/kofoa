e0cf7e3dff5789b4faf88b35e4d00408007c32a8
zoheir kabuli <zoheyrk@gmail.com>
2022-11-09 17:15:27 +0330

    complete posts and photos slider and mobile menu

--- js/app.js
+++ js/app.js
@@ -3,29 +3,70 @@ const heroSwiper = new Swiper(".hero-slider", {
     nextEl: ".hero-slider__next",
     prevEl: ".hero-slider__prev",
   },
+  pagination: {
+    el: ".hero-slider__dots",
+  },
   loop: true,
   spaceBetween: 20,
 });
 
 const homeNewsSwiper = new Swiper(".home-news__slider", {
-  slidesPerView: 4,
+  slidesPerView: 1,
   spaceBetween: 30,
   loop: true,
   navigation: {
     nextEl: ".home-news__slider-next",
     prevEl: ".home-news__slider-prev",
   },
+
+  breakpoints: {
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+      loop: true,
+      navigation: {
+        nextEl: ".home-news__slider-next",
+        prevEl: ".home-news__slider-prev",
+      },
+    },
+  },
 });
 
 // article slider
 
 const homeArticleSwiper = new Swiper(".home-articles__slider", {
-  slidesPerView: 3,
+  slidesPerView: 1,
   spaceBetween: 30,
   loop: true,
   navigation: {
     nextEl: ".home-articles__slider-next",
     prevEl: ".home-articles__slider-prev",
+  },
+
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      loop: true,
+      navigation: {
+        nextEl: ".home-articles__slider-next",
+        prevEl: ".home-articles__slider-prev",
+      },
+    },
+  },
+});
+
+// qa slidr
+
+const homeQaSwiper = new Swiper(".home-qa__slider", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  breakpoints: {
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+      loop: false,
+    },
   },
 });
 
@@ -33,19 +74,31 @@ const homeArticleSwiper = new Swiper(".home-articles__slider", {
 
 const bookSlider = new Swiper(".home-books__slider", {
   effect: "coverflow",
-  grabCursor: true,
+
   centeredSlides: true,
-  slidesPerView: "5",
+  slidesPerView: 3,
   loop: true,
   navigation: {
     nextEl: ".home-books__slider-next",
     prevEl: ".home-books__slider-prev",
   },
   coverflowEffect: {
-    rotate: 0,
-    stretch: -100,
-    depth: 200,
+    rotate: 40,
+    stretch: -20,
+    depth: 100,
     slideShadows: false,
+  },
+
+  breakpoints: {
+    768: {
+      slidesPerView: 5,
+      coverflowEffect: {
+        rotate: 40,
+        stretch: -100,
+        depth: 200,
+        slideShadows: false,
+      },
+    },
   },
 });
 
@@ -71,3 +124,41 @@ const players = Array.from(
 
 // Expose player so it can be used from the console
 // window.player = players;
+
+// mobile toggle
+
+const toggle = document.querySelector(".navbar__toggle");
+const menu = document.querySelector(".navbar__menu");
+
+toggle.addEventListener("click", () => {
+  menu.classList.toggle("open");
+});
+
+// home album
+
+if (window.matchMedia("(max-width: 768px)").matches) {
+  const homePosts = new Swiper(".home-posts__slider", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+    navigation: {
+      nextEl: ".home-posts__slider-next",
+      prevEl: ".home-posts__slider-prev",
+    },
+    pagination: {
+      el: ".home-posts__slider-dots",
+    },
+  });
+  const homeAlbum = new Swiper(".home-album__slider", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+    navigation: {
+      nextEl: ".home-album__slider-next",
+      prevEl: ".home-album__slider-prev",
+    },
+    pagination: {
+      el: ".home-album__slider-dots",
+    },
+  });
+}
